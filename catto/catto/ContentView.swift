92847d5259f9ecb359f94369809128173ce33931
Cris Messias <meunomeecriss@gmail.com>
2024-06-12 15:05:21 +0200

    commit initial

---
+++ catto/catto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  catto
+//
+//  Created by Cris Messias on 12/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ catto/catto/cattoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cattoApp.swift
+//  catto
+//
+//  Created by Cris Messias on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct cattoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
