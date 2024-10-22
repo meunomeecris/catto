34b1bcd40d60b06b2781fb0a32907f10b699f61f
Cris Messias <meunomeecriss@gmail.com>
2024-10-22 13:46:30 +0200

    splashScreen feature: 	- create animation for the splash screen

---
+++ catto/Catto/Features/Resource/SplashScreen.swift
@@ -0,0 +1,67 @@
+//
+//  SplashScreen.swift
+//  Catto
+//
+//  Created by Cris Messias on 19/10/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State private var showSplashScreen: Bool = true
+    @State private var showInfos: Bool = false
+    @State private var size = 0.2
+    @State private var opacity = 0.5
+
+    var body: some View {
+        ZStack {
+            Color.bgScreen
+                .ignoresSafeArea()
+            if showSplashScreen {
+                VStack {
+                    Image(.splashAvatar)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 180, height: 180)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.auxiliarCardCatBoard, lineWidth: 8))
+                        .shadow(radius: 12)
+                        .scaleEffect(size)
+                        .opacity(opacity)
+
+                    VStack() {
+                        Text("Catto")
+                            .font(.custom("InknutAntiqua-Medium", size: 40))
+                            .bold()
+                        Text("Prurr-crastinate Time!")
+                            .font(.custom("InknutAntiqua-Medium", size: 20))
+                            .multilineTextAlignment(.center)
+                            .lineSpacing(5)
+                    }
+                    .foregroundStyle(.auxiliarBrand)
+                    .opacity(showInfos ? 1 : 0)
+                    .animation(.easeIn(duration: 0.2), value: showInfos)
+                }
+            } else {
+                PostContestListView(viewModel: PostContestListViewModel(getContestList: PostContestListUseCase()))
+            }
+        }
+        .onAppear() {
+            withAnimation(.easeIn(duration: 1)) {
+                self.size = 1.0
+                self.opacity = 1.0
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                withAnimation {
+                    showInfos  = true
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                withAnimation(.smooth) {
+                    showSplashScreen = false
+                }
+            }
+        }
+    }
+}
